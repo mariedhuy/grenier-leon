3e6935e52ec0eb75bea20673ad898079af07b23f
Audrey Belliot <belliotaudrey@gmail.com>
2024-06-12 11:52:11 +0200

    see-requests

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+
+  def requests
+    @requests = Booking.where(status: "requested") # Il manque la logique de filtre sur les bookings qui concernent les items du user connecté
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Item.create(name: "plat à paella", category: "cuisine", picture: "https://www.mauviel-boutique.com/452-large_default/plat-a-paella-m-steel.jpg", description: "plat à paella")
+Booking.create(item_id: 1, status: "requested", start_date: 20240630, end_date: 20240710)
