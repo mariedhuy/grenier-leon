f551a9860bacf4593c0b62322946c9253c79eb8b
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-06-13 11:12:13 +0200

    update user form devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @items = Item.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
