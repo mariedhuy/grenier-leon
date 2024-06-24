f95ea4e8f5fd362101c5f729ff66430e68165f6e
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-06-25 00:44:13 +0200

    show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,12 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     @start_date = params[:start_date] || @booking.start_date
     @end_date = params[:end_date] || @booking.end_date
+    @markers = {
+      lat: @booking.item.user.latitude,
+      lng: @booking.item.user.longitude,
+      info_window_html: render_to_string(partial: "bookings/info_window", locals: {user: @booking.item.user}, formats: [:html]),
+      marker_html: render_to_string(partial: "bookings/marker", locals: {user: @booking.item.user}, formats: [:html])
+    }
   end
 
   def new
